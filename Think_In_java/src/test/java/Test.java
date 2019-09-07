019fae7328ed45f459d080409d6bddf25ecdc6c6
sivanliu <lygforbs0@gmail.com>
2019-09-07 15:57:12 +0800

    add sync

--- Think_In_java/src/test/java/Test.java
+++ Think_In_java/src/test/java/Test.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+class JoinThread implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("thread : " + Thread.currentThread().getName() + " --i: " + i);
+        }
+    }
+}
 
 public class Test {
+    public static void main(String[] args) {
+        JoinThread joinThread1 = new JoinThread();
+        Thread thread1 = new Thread(joinThread1);
+        Thread thread2 = new Thread(joinThread1);
+
+        thread1.start();
+
+        try {
+            thread1.join();
+            thread2.start();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("main : " + Thread.currentThread().getName() + " --i: " + i);
+        }
+    }
 }
--- Think_In_java/src/test/java/TrainTicket.java
+++ Think_In_java/src/test/java/TrainTicket.java
@@ -1,4 +1,57 @@
-package PACKAGE_NAME;
+class ThreadTrain2 implements Runnable {
+
+    private int count = 100;
+    public boolean flag = true;
+    private static Object oj = new Object();
+
+    @Override
+    public void run() {
+        if (flag) {
+            while (count > 0) {
+                synchronized (this) {
+                    if (count > 0) {
+                        try {
+                            Thread.sleep(50);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        System.out.println(Thread.currentThread().getName() + ", 出售第 " + (100 - count + 1) + " 票");
+                        count--;
+                    }
+                }
+            }
+        } else {
+            while (count > 0) {
+                sale();
+            }
+        }
+
+    }
+
+    public synchronized void sale() {
+
+        if (count > 0) {
+            try {
+                Thread.sleep(50);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + ", 出售第 " + (100 - count + 1) + " 票");
+            count--;
+        }
+    }
+
+}
 
 public class TrainTicket {
+    public static void main(String[] args) throws InterruptedException {
+        ThreadTrain2 threadTrain2 = new ThreadTrain2();
+        Thread thread1 = new Thread(threadTrain2, "1号窗口");
+        Thread thread2 = new Thread(threadTrain2, "2号窗口");
+        thread1.start();
+        Thread.sleep(50);
+        threadTrain2.flag = false;
+        thread2.start();
+    }
 }
